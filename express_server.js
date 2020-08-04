37238a1018c2009934d386fae74227f36ad452b1
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-08-04 01:49:58 +0000

    Deleted empty lines and useles code

--- express_server.js
+++ express_server.js
@@ -3,7 +3,6 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");         // This is for POST METHOD 
 app.use(bodyParser.urlencoded({extended: true}));  // This is for POST METHOD 
-
 app.set("view engine", "ejs")
 
 const urlDatabase = {
@@ -17,16 +16,16 @@ function generateRandomString() {
     let str = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
       for(let i = 0; i < 6; i++){
         result += str.charAt(Math.floor(Math.random() * str.length))
-  }
-  return result;
-  }
+      }
+return result;
+}
 
 /* Without this for POST, after submitting we wll have 404 */
 app.post("/urls", (req, res) => {
     const newKey = generateRandomString();
     urlDatabase[newKey] = req.body.longURL; //! Adding a pair of key - value to our object!!
     res.redirect(`/urls/${newKey}`);        //! Redirecting to our just generated shortURL page
-  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -34,7 +33,7 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-  });
+});
 
 app.get("/urls", (req, res) => {
     let templateVars = {urls: urlDatabase};
@@ -43,25 +42,23 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
-  });
-  
+});
+
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
-  });
+});
 
 // ===== After clicking on shortURL(PATH --> /u/:shortURL) redirecting to usuall WEB-SITE(longURL) ===== \\
-  app.get("/u/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
-    const longURL = templateVars.longURL
+app.get("/u/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
+  const longURL = templateVars.longURL
     res.redirect(longURL);
-  });
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-
-
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
