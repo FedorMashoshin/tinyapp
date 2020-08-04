1b3983289763690c293fc0055be9696a94d1f45b
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-08-04 01:47:11 +0000

    Add comment about Project

--- express_server.js
+++ express_server.js
@@ -44,12 +44,13 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
   });
-
+  
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
   });
 
+// ===== After clicking on shortURL(PATH --> /u/:shortURL) redirecting to usuall WEB-SITE(longURL) ===== \\
   app.get("/u/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
     const longURL = templateVars.longURL
