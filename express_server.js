f0d8b5e454a8a8f53e290d7cb7919d353277103f
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-08-05 03:16:52 +0000

    Add a Login-Logout form!

--- express_server.js
+++ express_server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");         // This is for POST METHOD 
+const cookieParser = require('cookie-parser');
 const { response } = require("express");
+
 app.use(bodyParser.urlencoded({extended: true}));  // This is for POST METHOD 
 app.set("view engine", "ejs")
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,6 +31,18 @@ app.post("/urls", (req, res) => {
     res.redirect(`/urls/${newKey}`);        //! Redirecting to our just generated shortURL page
 });
 
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect(`urls/`);
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username', req.body.username)
+  res.redirect(`urls/`);
+})
+
+
+
 
 app.get("/", (req, res) => {
   res.send("That is a homepage for TinyApp project!");
@@ -38,16 +53,25 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-    let templateVars = {urls: urlDatabase};
+    let templateVars = {
+      urls: urlDatabase,
+      username: req.cookies["username"]}; //! We took it from --> res.cookie('username', req.body.username);
     res.render("urls_index", templateVars);
+
 });
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]};
+    res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
+    let templateVars = { 
+      shortURL: req.params.shortURL, 
+      longURL:urlDatabase[req.params.shortURL],
+      username: req.cookies["username"]
+     };
     res.render("urls_show", templateVars);
 });
 
